22bd2b50b967b318d081ea1f09cd4efd51746618
Francis Kornu <fkay0450@gmail.com>
2023-02-26 22:33:51 +0000

    feat: user model setup and db population

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const {Users} = require('../../models');
+const { 
+    v1: uuidv1,
+    v4: uuidv4,
+  } = require('uuid');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({message:"user INFO"})
 })
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({message:"user INFO"})
+router.post('/', async(req, res, next) => {
+    const {firstname, lastname, email, phonenumber, password} = req.body;
+    try{
+        console.log(uuidv4());
+        const randomNumber = Math.floor(Math.random() * 10000);
+        const username = firstname.concat(randomNumber);
+        const referrence_no = uuidv4();
+        const roles = 'admin';
+
+        const user = await Users.create({firstname, lastname, email, phonenumber, password, username, referrence_no, roles})
+        res.status(200).json({
+            message:"user INFO",
+            user: user
+        })
+    }catch(err){
+        res.status(err.status || 500)
+        res.json({
+            error: {
+                message: err.message
+            }
+        })
+    }
 })
 
 router.get('/:id', (req, res, next) => {
